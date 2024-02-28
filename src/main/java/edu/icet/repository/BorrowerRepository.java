0b0c0c87881572021019bb8143043f928823e27c
DESKTOP-N4EBOK4\Dilshan <dilshanperera200325712934@gmail.com>
2024-02-28 11:49:57 +0530

    add is exiats user method

--- src/main/java/edu/icet/repository/BorrowerRepository.java
+++ src/main/java/edu/icet/repository/BorrowerRepository.java
@@ -4,4 +4,7 @@ import edu.icet.entity.BorrowerEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BorrowerRepository extends CrudRepository<BorrowerEntity,Long> {
+    public BorrowerEntity findByuserName(String userName);
+
+    Boolean existsByUserName(String userName);
 }
