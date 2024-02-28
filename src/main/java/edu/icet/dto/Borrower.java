e1cc85779ac659e7c2eac3825b4a56860d6e3221
DESKTOP-N4EBOK4\Dilshan <dilshanperera200325712934@gmail.com>
2024-02-28 08:35:52 +0530

    create  dto package for borrower

---
+++ src/main/java/edu/icet/dto/Borrower.java
@@ -0,0 +1,20 @@
+package edu.icet.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Borrower {
+    private Long id;
+    private String name;
+    private String contactNumber;
+    private String nic;
+    private String email;
+    private String address;
+    private String country;
+    private String username;
+    private String password;
+}
